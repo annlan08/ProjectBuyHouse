f5b59755315280e5902198df626d2f0bc2d7d7c3
Ben Lee <ben.lee@cloudnifier.com>
2023-04-03 03:16:29 +0800

    完成資料結接Data的CRUD Repository

---
+++ prjBuyHouse/Repository/HouseRepository.cs
@@ -0,0 +1,73 @@
+﻿using prjBuyHouse.Models;
+using prjBuyHouse.Repository.Interfaces;
+
+namespace prjBuyHouse.Repository
+{
+    public class HouseRepository:IHouseRepository
+    {
+        private HouseContext _context;
+        public HouseRepository(HouseContext context) 
+        {
+            this._context = context;
+        }
+
+        public async Task<HouseObject> GetHouseObjectById(int id)
+        {
+            var result=this._context.HouseObjects.Where(x=>x.FId==id).FirstOrDefault();
+            return result;
+        }
+
+        public async Task<bool> CreateNewHouseObject(HouseObject houseObject)
+        {
+            try
+            {
+                this._context.HouseObjects.Add(houseObject);
+                this._context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateHouseObject(int id, HouseObject houseObject)
+        {
+            try
+            {
+                var targetHouse = this._context.HouseObjects.Where(x => x.FId == id).FirstOrDefault();
+                targetHouse.FName= houseObject.FName;
+                targetHouse.FPrice= houseObject.FPrice;
+                targetHouse.FDescription= houseObject.FDescription;
+                this._context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteHouseObjectById(int id)
+        {
+            try 
+            {
+                var targetHouse = this._context.HouseObjects.Where(x => x.FId == id).FirstOrDefault();
+                if(targetHouse!=null)
+                {
+                    this._context.HouseObjects.Remove(targetHouse);
+                    this._context.SaveChanges();
+                    return true;
+                }
+                else
+                {
+                    throw new NullReferenceException();
+                }
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+    }
+}
--- prjBuyHouse/Repository/Interfaces/IHouseRepository.cs
+++ prjBuyHouse/Repository/Interfaces/IHouseRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace prjBuyHouse.Repository.Interfaces
+﻿using prjBuyHouse.Models;
+
+namespace prjBuyHouse.Repository.Interfaces
 {
     public interface IHouseRepository
     {
+        Task<HouseObject> GetHouseObjectById(int id);
+        Task<bool> CreateNewHouseObject(HouseObject houseObject);
+        Task<bool> UpdateHouseObject(int id, HouseObject houseObject);
+        Task<bool> DeleteHouseObjectById(int id);
     }
 }
